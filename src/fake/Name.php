5a40598edb85dda61ff44281ffc0b4cdfa2e139f
Nguyen Thang <nguyenthang011099.hust@gmail.com>
2019-08-02 14:57:44 +0700

    done

--- src/fake/Name.php
+++ src/fake/Name.php
@@ -1,16 +1,21 @@
 <?php
 namespace fake;
 class Name{
+
     public function random()
     {
         $txt= fopen("name.txt", "r") or die("Unable to open file!");
         $result= [];
+
+
         while(!feof($txt))  {
             $result[]= fgets($txt);
+
 
         }
         //if you write  $result[array_rand($result)]);return array_rand($result) it will return key  of array
         fclose($txt);
-        return ($result[array_rand($result)]);
+        return $result[array_rand($result)];
     }
+
 }
--- test.php
+++ test.php
@@ -2,4 +2,4 @@
 require_once "vendor/autoload.php";
 use fake\Name;
 $obj = new Name();
-echo $obj->random();
+echo ($obj->random());
